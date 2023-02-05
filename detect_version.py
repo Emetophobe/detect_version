9c7dcd01dc0282a949350fa98b27a9aa923d1b5d
Mike Cunningham <Mike933@gmail.com>
2023-02-05 13:36:37 -0500

    Added modules up to Python 3.12 (alpha)
    Added a few missing modules for older version.

--- detect_version.py
+++ detect_version.py
@@ -19,10 +19,12 @@ PYTHON38 = (3, 8, 0)
 PYTHON39 = (3, 9, 0)
 PYTHON310 = (3, 10, 0)
 PYTHON311 = (3, 11, 0)
+PYTHON312 = (3, 12, 0)
 
 
 """ TODO list:
 
+    0. RecursionError added (Python 3.5)
     1. Check type annotations (Python 3.6 - PEP 526)
     2. Underscores in numeric literals (Python 3.6 - PEP 515)
     3. Asynchronous Generators (Python 3.6 - PEP 525)
@@ -32,6 +34,9 @@ PYTHON311 = (3, 11, 0)
     7. f-strings support for self-documenting expressions; i.e f"{var=}" (Python 3.8)
     8. type hinting generics in standard collections (Python 3.9 - PEP 585)
     9. structural pattern matching aka match statement (Python 3.10 - PEP 634)
+    10. new built-in async functions 'aiter', and 'anext' (Python 3.10)
+    11. ExceptionGroup added (Python 3.11 - PEP 654)
+    12. Exceptions can be enriched with notes (Python 3.11 - PEP 678)
 """
 
 
@@ -50,6 +55,7 @@ MODULE_CHANGES = {
         'concurrent': ['futures'],
         'contextlib': ['ContextDecorator'],
         'datetime': ['timezone'],
+        'dis': ['DELETE_DEREF', 'code_info', 'show_code'],
         'functools': ['lru_cache'],
         'gzip': ['compress', 'decompress'],
         'hashlib': ['algorithms_available', 'algorithms_guaranteed'],
@@ -78,14 +84,15 @@ MODULE_CHANGES = {
         'inspect': ['signature', 'Signature', 'Parameter', 'BoundArguments'],
         'lzma': [],
         'math': ['log2'],
-        'os': ['SF_NODISKIO', 'SF_MNOWAIT', 'SF_SYNC', 'readv', 'writev' 'pread', 'pwrite',
-               'sendfile'],
+        'os': ['SF_NODISKIO', 'SF_MNOWAIT', 'SF_SYNC', 'XATTR_SIZE_MAX', 'XATTR_CREATE',
+               'XATTR_REPLACE', 'readv', 'writev' 'pread', 'pwrite', 'sendfile',
+               'getxattr', 'listxattr', 'removexattr', 'setxattr'],
         'select': ['devpoll'],
         'signal': ['pthread_kill'],
         'socket': ['CMSG_LEN', 'CMSG_SPACE', 'fromshare', 'if_nameindex', 'if_nametoindex',
                    'if_indextoname', 'recvmsg', 'recvmsg_into', 'sendmsg', 'sethostname',
                    'share'],
-        'sys': ['implementation', '_debugmallocstats'],
+        'sys': ['_debugmallocstats', 'implementation', 'thread_info'],
         'threading': ['get_ident'],
         'time': ['CLOCK_REALTIME', 'clock_settime'],
         'typing': ['SimpleNamespace'],
@@ -99,7 +106,7 @@ MODULE_CHANGES = {
         'audioop': ['byteswap'],
         'asyncio': [],
         'contextlib': ['redirect_stdout, suppress'],
-        'dis': ['Instruction', 'get_instruction', 'stack_effect'],
+        'dis': ['LOAD_CLASSDEREF', 'Bytecode', 'Instruction', 'get_instructions', 'stack_effect'],
         'email': ['as_bytes'],
         'ensurepip': [],
         'enum': [],
@@ -139,6 +146,7 @@ MODULE_CHANGES = {
                             'Generator'],
         'contextlib': ['redirect_stderr'],
         'html': ['HTTPStatus'],
+        'inspect': ['iscoroutine', 'isawaitable'],
         'math': ['inf', 'nan'],
         'os': ['DirEntry', 'scandir'],
         'socket': ['CAN_RAW_FD_FRAMES', 'sendfile'],
@@ -146,7 +154,9 @@ MODULE_CHANGES = {
         'traceback': ['TracebackException', 'StackSummary', 'FrameSummary', 'walk_stack',
                       'walk_tb'],
         'types': ['CoroutineType', 'coroutine'],
+        'typing': [],
         'urllib.request': ['HTTPPasswordMgrWithPriorAuth'],
+        'zipapp': [],
     },
 
     # Python 3.6
@@ -156,9 +166,11 @@ MODULE_CHANGES = {
         'cmath': ['tau', 'inf', 'infj', 'nan', 'nanj'],
         'collections.abc': ['AsyncGenerator', 'Collection', 'Reversible'],
         'contextlib': ['AbstractContextManager'],
+        'dis': ['CALL_FUNCTION_EX', 'FORMAT_VALUE'],
         'enum': ['Flag', 'IntFlag'],
         'hashlib': ['blake2b', 'blake2s', 'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512',
                     'shake_128', 'shake_256', 'scrypt'],
+        'inspect': ['isasyncgenfunction', 'isasyncgen'],
         'math': ['tau'],
         'os': ['fspath', 'PathLike'],
         'random': ['choices'],
@@ -185,6 +197,7 @@ MODULE_CHANGES = {
         'crypt': ['METHOD_BLOWFISH'],
         'dataclasses': [],
         'datetime': ['datetime.fromisoformat'],
+        'dis': ['LOAD_METHOD'],
         'gc': ['freeze', 'unfreeze', 'get_freeze_count'],
         'http.server': ['ThreadingHTTPServer'],
         'importlib.resources': [],
@@ -207,6 +220,8 @@ MODULE_CHANGES = {
         'ast': ['PyCF_ALLOW_TOP_LEVEL_AWAIT', 'PyCF_TYPE_COMMENTS', 'get_source_segment'],
         'datetime': ['date.fromisocalendar', 'datetime.fromisocalendar'],
         'curses': ['ncurses_version'],
+        'fcntl': ['F_ADD_SEALS', 'F_GET_SEALS', 'F_SEAL_GROW', 'F_SEAL_SHRINK',
+                  'F_SEAL_SEAL', 'F_SEAL_WRITE'],
         'functools': ['cached_property', 'singledispatchmethod'],
         'gettext': ['pgettext', 'dpgettext', 'npgettext', 'dnpgettext'],
         'gzip': ['BadGzipFile'],
@@ -236,7 +251,7 @@ MODULE_CHANGES = {
         'ast': ['unparse'],
         'asyncio': ['PidfdChildWatcher', 'loop.shutdown_default_executor', 'to_thread'],
         'curses': ['get_escdelay', 'set_escdelay', 'get_tabsize', 'set_tabsize'],
-        'fcntl': ['F_OFD_GETLK', 'F_OFD_SETLK', 'F_OFD_SETLKW'],
+        'fcntl': ['F_GETPATH', 'F_OFD_GETLK', 'F_OFD_SETLK', 'F_OFD_SETLKW',],
         'gc': ['is_finalized'],
         'graphlib': [],
         'importlib': ['resources.files', 'resources.as_file'],
@@ -255,22 +270,70 @@ MODULE_CHANGES = {
 
     # Python 3.10
     PYTHON310: {
-        'os': ['RWF_APPEND'],
-        'threading': ['__excepthook__'],
-        'types': ['NoneType', 'NotImplementedType'],
+        'base64': ['b32hexencode', 'b32hexdecode'],
+        'codecs': ['unregister'],
+        'contextlib': ['AsyncContextDecorator', 'aclosing'],
+        'curses': ['has_extended_color_support'],
+        'dis': ['MATCH_CLASS'],
+        'fcntl': ['F_GETPIPE_SZ', 'F_SETPIPE_SZ'],
+        'importlib.metadata': ['packages_distributions'],
+        'inspect': ['get_annotations'],
+        'itertools': ['pairwise'],
+        'os': ['EFD_CLOEXEC', 'EFD_NONBLOCK', 'EFD_SEMAPHORE', 'O_EVTONLY', 'O_FSYNC',
+               'O_SYMLINK', 'O_NOFOLLOW_ANY', 'RWF_APPEND', 'SPLICE_F_MOVE',
+               'SPLICE_F_NONBLOCK', 'SPLICE_F_MORE', 'eventfd', 'eventfd_read',
+               'eventfd_write', 'splice'],
+        'platform': ['freedesktop_os_release'],
+        'statistics': ['covariance', 'correlation', 'linear_regression'],
+        'sys': ['orig_argv', 'stdlib_module_names'],
+        'threading': ['__excepthook__', 'getprofile', 'gettrace'],
+        'types': ['EllipsisType', 'NoneType', 'NotImplementedType', 'UnionType'],
         'typing': ['is_typeddict'],
+        'xml.sax.handler': ['LexicalHandler'],
     },
 
     # Python 3.11
     PYTHON311: {
+        'asyncio': ['Barrier', 'BrokenBarrierError', 'Runner', 'TaskGroup', 'Timeout',
+                    'timeout'],
+        'contextlib': ['chdir'],
+        'datetime': ['datetime.UTC'],
+        'dis': ['ASYNC_GEN_WRAP', 'COPY_FREE_VARS', 'CALL', 'COPY', 'KW_NAMES', 'PRECALL',
+                'PUSH_NULL', 'SEND', 'SWAP', 'RESUME', 'RETURN_GENERATOR', 'Positions'],
+        'enum': ['EnumType', 'ReprEnum', 'StrEnum', 'global_enum', 'member', 'nonmember',
+                 'property', 'show_flag_values', 'verify'],
+        'fcntl': ['F_DUP2FD', 'F_DUP2FD_CLOEXEC'],
         'hashlib': ['file_digest'],
+        'inspect': ['getmembers_static', 'ismethodwrapper'],
+        'locale': ['getencoding'],
+        'logging': ['getLevelNamesMapping'],
         'math': ['cbrt', 'exp2'],
+        'operator': ['call'],
         'os': ['SF_NOCACHE'],
         'socket': ['SCM_CREDS2', 'LOCAL_CREDS', 'LOCAL_CREDS_PERSISTENT',
                    'SO_INCOMING_CPU'],
+        'sqlite3': ['Blob'],
+        'sys': ['exception'],
+        'string': ['get_identifiers', 'is_valid'],
         'tomllib': [],
-        'typing': ['NotRequired'],
+        'typing': ['TypeVarTuple', 'Required', 'Never', 'NotRequired', 'Self', 'LiteralString',
+                   'assert_never', 'assert_type', 'get_overloads', 'clear_overloads',
+                   'dataclass_transform', 'reveal_type'],
+        'unittest': ['enterModuleContext'],
+        'wsgiref.types': [],
+        'zipfile': ['ZipFile.mkdir'],
     },
+
+    # Python 3.12 (Preliminary alpha changes)
+    PYTHON312: {
+        'dis': ['hasarg'],
+        'inspect': ['markcoroutinefunction'],
+        'math': ['sumprod'],
+        'os': ['PIDFD_NONBLOCK', 'path.isjunction', 'path.splitroot'],
+        'sys': ['activate_stack_trampoline', 'deactivate_stack_trampoline',
+                'is_stack_trampoline_active'],
+        'threading': ['settrace_all_threads', 'setprofile_all_threads'],
+    }
 }
 
 
