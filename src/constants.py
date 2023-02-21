72cd490ba5dcbdd6334f7984801b87cfede58267
Mike Cunningham <Mike933@gmail.com>
2023-02-21 05:57:00 -0500

    Re-organize constants.

--- src/constants.py
+++ src/constants.py
@@ -5,10 +5,7 @@
 # This script is designed for Python 3.x
 MINIMUM_VERSION = '3.0'
 
-
-# General language changes
-
-# Python 3.1: Multiple "with" statements
+# Python 3.1: Multiple "with" clauses
 MULTIPLE_CONTEXT_MANAGERS = 'multiple context managers'
 
 # Python 3.3: PEP 380 - Syntax for Delegating to a Subgenerator
@@ -29,23 +26,20 @@ AITER_AND_ANEXT = 'aiter and anext'
 # Python 3.6: PEP 530 - Asynchronous Comprehensions
 ASYNC_COMPREHENSIONS = 'asynchronous comprehensions'
 
-# Python 3.8: PEP 570 - Python Positional-Only Parameters
+# Python 3.6: PEP 526 - Syntax for Variable Annotations
+VARIABLE_ANNOTATIONS = 'variable annotations'
+
+# Python 3.8: PEP 570 - Positional-Only Parameters
 POSONLY_ARGUMENTS = 'positional-only arguments'
 
 # Python 3.8: PEP 572 - Assignment Expressions
 WALRUS_OPERATOR = 'assignment expressions'
-
-# Python 3.10: PEP 622 - Structural Pattern Matching
-MATCH_STATEMENT = 'pattern matching'
-
-
-# Annotation-related changes
-
-# Python 3.6: PEP 526 - Syntax for Variable Annotations
-VARIABLE_ANNOTATIONS = 'variable annotations'
 
 # Python 3.9: PEP 585 - Type Hinting Generics In Standard Collections
 GENERIC_TYPE_HINTS = 'type hinting generics'
 
 # Python 3.10: PEP 604 - Allow writing union types as X | Y
 UNION_TYPE_HINTING = 'union type hinting'
+
+# Python 3.10: PEP 622 - Structural Pattern Matching
+MATCH_STATEMENT = 'pattern matching'
